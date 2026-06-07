b1f8952b8a3a73a1d470802abcceb58e0c6d7ec9
alokkumarverma1 <alokkumarverma505@gmail.com>
2026-06-07 13:46:34 +0530

    feat: create modern responcive hero section of home page

---
+++ src/pages/others/homecard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Homecard({number , data}){
+    return(<>
+    <div className="homecard flex justify-center flex-col items-center gap-3 sm:h-30 h-20 sm:w-50 w-25 p-1 rounded-2xl  border-white shadow-gray-600 shadow-md ">
+     <h1 className="font-bold sm:text-3xl text-[20px] metal-green">{number}</h1>
+     <p className="sm:text-2xl text-center text-[10px]">{data}</p>
+    </div>
+    </>)
+}
+
+export default Homecard;
